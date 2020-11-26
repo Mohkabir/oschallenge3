44251658efe7d34d188a60b528dc8b60b27cc3f3
mohammed kabir <kbshowkb@gmail.com>
2020-11-26 03:46:02 -1200

    up date

--- script.js
+++ script.js
@@ -2,7 +2,31 @@ $(document).ready(function () {
   $('.menu').click(function(){
     $('.toggle_nav').toggle();
   });
+
+
+  $('.main_top').click(function(){
+    let geturl = window.location.href ;
+    let getlocation = geturl.substring(geturl.lastIndexOf('#')+1);
+    if(getlocation == 'home'){
+      $('html, body').animate({
+        scrollTop: $('#'+ getlocation).offset().top
+      },500);
+    }
+  });
 });
+
+window.addEventListener('scroll', function(){
+  let top = document.querySelector('.main_top');
+
+  let scrolling = document.documentElement.scrollTop;
+
+  if(scrolling < 160){
+    top.style.display = 'none';
+  }else {
+    top.style.display = 'block';
+  }
+})
+
 
 
 
